750b1778ae2554d0797cbb0d3685ef52a89f3f41
Tamilarasan-12 <141328399+Tamilarasan-12@users.noreply.github.com>
2023-09-13 23:05:53 -0700

    Update CHALLENGE_2.1.py

--- UNIT_2_CHALLENGES/CHALLENGE_2.1.py
+++ UNIT_2_CHALLENGES/CHALLENGE_2.1.py
@@ -1,3 +1,5 @@
+''' Implement a class called Player that represents a cricket player. The Player class should have a method called play() which prints "The player is playing cricket. Derive two classes, Batsman and Bowler, from the Player class. Override the play() method in each derived class to print "The batsman is batting" and "The bowler is bowling", respectively. Write a program to create objects of both the Batsman and Bowler classes and call the play() method for each object. '''
+
 class BankAccount:
     def __init__(self, account_number, account_holder_name, initial_balance=0):
         self.__account_number = account_number
